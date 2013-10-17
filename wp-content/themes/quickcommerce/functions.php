0b6f5b00a9862159a793ff6ee475cafc3eab6c2f
Bernard Peh <bpeh@sitecritic.net>
2013-10-17 13:34:08 +1100

    uses woocommerce v2.0.16 instead.

--- wp-content/themes/quickcommerce/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Add logout link to naviagation menu
-function add_logout_link_to_menu( $items, $args ){
-	$link = '<a href="'.wp_logout_url(home_url()).'" title="'.__( 'Logout' ) .'">'.__( 'Logout' ).'</a>';
-	return $items.= '<li id="log-out-link" class="menu-item">'. $link . '</li>';
-}
-if (is_user_logged_in()) {
-	add_filter( 'wp_nav_menu_items', 'add_logout_link_to_menu', 10, 2 );
-}
